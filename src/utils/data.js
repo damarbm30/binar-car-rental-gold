b880b2261e011c7102ac09ddd38631f0a9b7d8e4
Damar Buana Murti <damarbm30@gmail.com>
2023-01-01 19:03:24 +0700

    add dummy data

---
+++ src/utils/data.js
@@ -0,0 +1,26 @@
+export const testimonials = [
+  {
+    id: 1,
+    avatar: "https://i.pravatar.cc/80",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 1)",
+    info: "John Dee 32, Bromo",
+  },
+  {
+    id: 2,
+    avatar: "https://i.pravatar.cc/80",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 2)",
+    info: "John Dee 32, Bromo",
+  },
+  {
+    id: 3,
+    avatar: "https://i.pravatar.cc/80",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 3)",
+    info: "John Dee 32, Bromo",
+  },
+  {
+    id: 4,
+    avatar: "https://i.pravatar.cc/80",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 4)",
+    info: "John Dee 32, Bromo",
+  },
+];
