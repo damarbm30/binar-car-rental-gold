58261836d154fadccf456cb9edd37ece48bbff5b
Damar Buana Murti <damarbm30@gmail.com>
2023-01-02 20:15:56 +0700

    add CTA

--- src/pages/CTA/CTA.jsx
+++ src/pages/CTA/CTA.jsx
@@ -1,4 +1,21 @@
+import "./CTA.css";
+
 const CTA = () => {
-  return <div>CTA</div>;
+  return (
+    <section className="container section-pt">
+      <div className="d-flex flex-column align-items-center bg-blue text-white cta">
+        <h1 className="text-center">Sewa Mobil di Yogyakarta Sekarang</h1>
+        <p className="mt-3 text-center">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua.
+        </p>
+        <div>
+          <button role="button" className="btn btn btn-green text-white fw-bold mt-5">
+            Mulai Sewa Mobil
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default CTA;
