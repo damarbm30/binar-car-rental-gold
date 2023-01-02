f617b780b27df5f94d35884d8d2b5d22449cf6f7
Damar Buana Murti <damarbm30@gmail.com>
2023-01-02 20:21:49 +0700

    button margin for mobile devices

--- src/pages/CTA/CTA.jsx
+++ src/pages/CTA/CTA.jsx
@@ -4,13 +4,13 @@ const CTA = () => {
   return (
     <section className="container section-pt">
       <div className="d-flex flex-column align-items-center bg-blue text-white cta">
-        <h1 className="text-center">Sewa Mobil di Yogyakarta Sekarang</h1>
+        <h1 className="text-center fs-sm-3">Sewa Mobil di Yogyakarta Sekarang</h1>
         <p className="mt-3 text-center">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
           magna aliqua.
         </p>
         <div>
-          <button role="button" className="btn btn btn-green text-white fw-bold mt-5">
+          <button role="button" className="btn btn btn-green text-white fw-bold mt-md-5 mt-3">
             Mulai Sewa Mobil
           </button>
         </div>
