4d206724af76968c85495bd39680e03672c71131
Damar Buana Murti <damarbm30@gmail.com>
2023-01-04 18:09:03 +0700

    add column

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,17 @@
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="container section-pt section-pb">
+      <div className="row">
+        <div className="col-md-2">
+          <p>Jalan Suroyo No. 161 Mayangan Kota Probolonggo 672000</p>
+          <p>binarcarrental@gmail.com</p>
+          <p>081-233-334-808</p>
+        </div>
+        <div className="col-md-2 offset-md-1">Col 1</div>
+        <div className="col-md-3 offset-md-1">Col 4</div>
+        <div className="col-md-2 offset-md-1">Col 2</div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
