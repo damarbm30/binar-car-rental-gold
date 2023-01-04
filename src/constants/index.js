7661a0ef4193d60b617832985d9453b8c98ef167
Damar Buana Murti <damarbm30@gmail.com>
2023-01-04 18:08:53 +0700

    add text for faqs

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+export const faqs = [
+  {
+    id: "accord-1",
+    title: "Apa saja syarat yang dibutuhkan?",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora impedit culpa reprehenderit nesciunt deleniti iure mollitia omnis! Sapiente.",
+  },
+  {
+    id: "accord-2",
+    title: "Berapa hari minimal sewa mobil lepas kunci?",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora impedit culpa reprehenderit nesciunt deleniti iure mollitia omnis! Sapiente.",
+  },
+  {
+    id: "accord-3",
+    title: "Berapa hari sebelumnya sebaiknya booking sewa mobil?",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora impedit culpa reprehenderit nesciunt deleniti iure mollitia omnis! Sapiente.",
+  },
+  {
+    id: "accord-4",
+    title: "Apakah ada biaya antarjemput?",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora impedit culpa reprehenderit nesciunt deleniti iure mollitia omnis! Sapiente.",
+  },
+  {
+    id: "accord-5",
+    title: "Bagaimana jika terjadi kecelakaan?",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis temporibus tempora impedit culpa reprehenderit nesciunt deleniti iure mollitia omnis! Sapiente.",
+  },
+];
