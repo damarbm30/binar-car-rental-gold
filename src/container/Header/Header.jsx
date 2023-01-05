668fb7ccf1fc70e7735c59e1c2c74f793f410e6b
Damar Buana Murti <damarbm30@gmail.com>
2023-01-05 20:14:01 +0700

    add conditional rendering for button

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import "./Header.css";
 import { car } from "../../assets/index";
 
-const Header = () => {
+const Header = ({ isHome }) => {
   return (
     <>
       <header className="header">
@@ -13,7 +13,7 @@ const Header = () => {
                 Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau.
                 Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.
               </p>
-              <button className="btn btn-green text-white fw-bold mt-3">Mulai Sewa Mobil</button>
+              {isHome && <button className="btn btn-green text-white fw-bold mt-3">Mulai Sewa Mobil</button>}
             </div>
             <img src={car} alt="car" className="col-xs-12 col header__img" />
           </div>
