e815905f06613e3a89bbcf3aa7384ff2c87ea055
Damar Buana Murti <damarbm30@gmail.com>
2023-01-08 17:35:03 +0700

    add userLogin function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
 
-import { Footer, Navbar } from "./components";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Footer, Navbar } from "./components";
 import { Cars, Home } from "./pages";
+import { userLogin } from "./services/auth";
 
 function App() {
+  useEffect(() => {
+    userLogin();
+  }, []);
+
   return (
     <>
       <Navbar />
