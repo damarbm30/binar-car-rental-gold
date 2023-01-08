c1713414e959ff9f213b2307af7e49b5c7b24f82
Damar Buana Murti <damarbm30@gmail.com>
2023-01-08 18:18:36 +0700

    fix endpoint variable

--- src/services/carServices.js
+++ src/services/carServices.js
@@ -9,11 +9,13 @@ export const getCars = async (name, category, minPrice, maxPrice, isRented) => {
           "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY3MzEwMjIzM30.x11FIoC6ZzAlrLVi_D0R46Kvq4L7DEUkXVR5TQvXXZk",
       },
     };
+
     const result = await api.get(
       `/admin/v2/car?name=${name}&category=${category}&isRented=${isRented}&minPrice=${minPrice}&maxPrice=${maxPrice}`,
       header
     );
-    console.log(result);
+
+    console.log(result.data.cars);
   } catch (error) {
     console.log(error);
   }
