58ba9d9480e254cd99818f07a192d9d524ec153b
Damar Buana Murti <damarbm30@gmail.com>
2023-01-16 12:59:55 +0700

    add Car component

---
+++ src/pages/Cars/CarList/Car.jsx
@@ -0,0 +1,29 @@
+const Car = ({ id, name, price, image }) => {
+  const formatter = new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+    minimumFractionDigits: 0,
+  });
+
+  const formattedPrice = formatter.format(price);
+
+  return (
+    <div className="col-4">
+      <div className="card">
+        <div className="card-body d-flex flex-column gap-1">
+          <div className="d-flex flex-column align-items-center justify-content-center">
+            <img src={image} alt={name} height={160} className="rounded" />
+          </div>
+          <p className="mt-4">{name}</p>
+          <p className="fw-bold">{formattedPrice} / hari</p>
+          <p className="fw-bold">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Car;
