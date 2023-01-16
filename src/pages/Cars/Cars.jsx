e30923ed211fc7b6df2c8525322502fe326b5104
Damar Buana Murti <damarbm30@gmail.com>
2023-01-16 12:59:46 +0700

    add CarList component

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,11 +1,17 @@
-import { Form } from "./Form";
+import { useState } from "react";
+
 import { Header } from "../../components";
+import Search from "./Search";
+import CarList from "./CarList";
 
 const Cars = () => {
+  const [cars, setCars] = useState([]);
+
   return (
     <main>
       <Header />
-      <Form />
+      <Search setCars={setCars} />
+      <CarList cars={cars} setCars={setCars} />
     </main>
   );
 };
