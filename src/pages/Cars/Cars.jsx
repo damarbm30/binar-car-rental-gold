111286b8cf645f1677ef445e5df2475fc2089a88
Damar Buana Murti <damarbm30@gmail.com>
2023-01-16 19:11:54 +0700

    add isFiltered and setIsFiltered props

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react";
+
 import { Header, Search } from "../../components";
 import CarList from "./CarList";
 
-const Cars = ({ cars, setCars }) => {
+const Cars = ({ cars, setCars, isFiltered, setIsFiltered }) => {
   return (
     <main>
-      <Header />
-      <Search setCars={setCars} />
+      <Header isFiltered={isFiltered} />
+      <Search setCars={setCars} setIsFiltered={setIsFiltered} />
       <CarList cars={cars} setCars={setCars} />
     </main>
   );
