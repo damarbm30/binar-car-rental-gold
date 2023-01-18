a4f90ab6e74d62e16f82c50174e8d8f2be32854f
Damar Buana Murti <damarbm30@gmail.com>
2023-01-18 21:01:09 +0700

    change avatar for each item

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,28 +1,28 @@
 export const testimonials = [
   {
     id: 1,
-    avatar: "https://i.pravatar.cc/80",
+    avatar: "https://i.pravatar.cc/80?img=1",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 1)",
     info: "John Dee 32, Bromo",
     isActive: "active",
   },
   {
     id: 2,
-    avatar: "https://i.pravatar.cc/80",
+    avatar: "https://i.pravatar.cc/80?img=2",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 2)",
     info: "John Dee 32, Bromo",
     isActive: "inactive",
   },
   {
     id: 3,
-    avatar: "https://i.pravatar.cc/80",
+    avatar: "https://i.pravatar.cc/80?img=3",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 3)",
     info: "John Dee 32, Bromo",
     isActive: "inactive",
   },
   {
     id: 4,
-    avatar: "https://i.pravatar.cc/80",
+    avatar: "https://i.pravatar.cc/80?img=4",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod (Review 4)",
     info: "John Dee 32, Bromo",
     isActive: "inactive",
