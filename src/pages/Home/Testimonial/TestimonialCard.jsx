30a0ff1a0670e11cfb4b824e26c5328e2f45c344
Damar Buana Murti <damarbm30@gmail.com>
2023-01-18 21:02:03 +0700

    remove active

--- src/pages/Home/Testimonial/TestimonialCard.jsx
+++ src/pages/Home/Testimonial/TestimonialCard.jsx
@@ -1,17 +1,15 @@
 import "./Testimonial.css";
 
-const TestimonialCard = ({ avatar, body, info, isActive }) => {
+const TestimonialCard = ({ avatar, body, info }) => {
   return (
-    <div className={`carousel-item ${isActive === "active" ? `active` : ""}`}>
-      <div className="col-md-6 col-xs-12 mx-auto">
-        <div className="card">
-          <div className="card-body d-flex align-items-center gap-3">
-            <img src={avatar} alt="avatar" className="rounded-circle" />
-            <div className="d-flex flex-column gap-2">
-              <p>&#x2B50;&#x2B50;&#x2B50;&#x2B50;&#x2B50;</p>
-              <p className="fw-bold">{body}</p>
-              <p>{info}</p>
-            </div>
+    <div className="col-lg-4 col-md-12">
+      <div className="card testimonial-card mx-4">
+        <div className="card-body d-flex align-items-center gap-3">
+          <img src={avatar} alt="avatar" className="rounded-circle" />
+          <div className="d-flex flex-column gap-2">
+            <p>&#x2B50;&#x2B50;&#x2B50;&#x2B50;&#x2B50;</p>
+            <p className="fw-bold text-wrap">{body}</p>
+            <p>{info}</p>
           </div>
         </div>
       </div>
